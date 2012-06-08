14720e4bd48e0b36e5b2cbe65652f104d0ad8ea5
Tom Schuster <evilpies@gmail.com>
2012-06-09 00:05:41 +0200

    add bottom up rand

--- harden.cpp
+++ harden.cpp
@@ -10,7 +10,7 @@ typedef NTSTATUS (NTAPI *_NtSetInformationProcess)(
  * Enable or disable DEP for the current process.
  * Once you use used this with Permanent the option,
  *  the DEP setting can't be changed.
- * You should not call this function on X64 unless you are in an Wow64Process, 
+ * You should not call this function on x64 unless you are in an Wow64Process, 
  *  because DEP is always enabled.
  */
 bool
@@ -33,11 +33,34 @@ SetDEP(bool Enable, bool Permanent)
 }
 
 /*
- * ASLR works by allocating the preffered image base of an to be loaded image,
+ * ASLR works by allocating the preferred image base of an to be loaded image,
  * this forces the windows kernel to load it at a different address.
  */
 bool
 SetMandoryASLR(bool Enable)
 {
-    
+    return false;
+}
+
+/*
+ * This increases the entropy of DLL base addresses even further.
+ * EMET seems to disable this for Vista and up.
+ */
+void
+EnableBottomUpRand()
+{
+    ULONG Seed = GetCurrentProcessId() ^ GetTickCount();
+    ULONG Range = RtlRandom(&Seed) & 255;
+
+    for (ULONG i = 0; i < Range, i++) {
+        PVOID Address = NULL;
+        SIZE_T Size = 64 * 1024;
+        (void) NtAllocateVirtualMemory(GetCurrentProcess(), Address, 0, &Size, MEM_RESERVE, PAGE_NOACCESS);
+    }
+}
+
+bool
+SetUpProtection()
+{
+    return true;
 }
